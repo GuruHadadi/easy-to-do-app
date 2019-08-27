17b8602af93d2050649e8d8ade0c831f06aa42a0
GuruHadadi500 <guru.hadadi@qantasloyalty.com>
2019-08-27 12:35:02 +1000

    started adding header plus little bit of clean up

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.css';
+
+const header = (props) => {
+    return (
+        <header className={'header'}>Easy To Do App</header>
+    )
+};
+
+export default header;
